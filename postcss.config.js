ba737fbae36f973c607169758db2b84cdc828081
Lucidreline <casta.ma502@gmail.com>
2025-09-29 18:34:26 -0700

    Fixed the styling.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// ✨ postcss.config.js
 export default {
     plugins: {
-        '@tailwindcss/postcss': {},
+        tailwindcss: {}, // ✨ Corrected this line  corrects
         autoprefixer: {},
     },
 }
